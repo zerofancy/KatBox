f04128d474514ce9e230e35766c4ac788091264b
zerofancy <ntutn.top@gmail.com>
2025-04-11 15:25:40 +0800

    feat(ChatAreaViewModel): 添加模型列表和选中模型状态管理

--- composeApp/src/commonMain/kotlin/top/ntutn/katbox/ChatAreaViewModel.kt
+++ composeApp/src/commonMain/kotlin/top/ntutn/katbox/ChatAreaViewModel.kt
@@ -13,9 +13,17 @@ class ChatAreaViewModel: ViewModel() {
     private val _historyStateFlow = MutableStateFlow("")
     val historyStateFlow: StateFlow<String> = _historyStateFlow
 
+    private val _modelsStateFlow = MutableStateFlow<List<Model>>(emptyList())
+    private val _selectedModel = MutableStateFlow<Model?>(null)
+    val selectedModel: StateFlow<Model?> = _selectedModel
+    val modelsStateFlow: StateFlow<List<Model>> = _modelsStateFlow
+
     fun fetchModels() = viewModelScope.launch {
         val response = getPlatform().httpClient.get("http://localhost:11434/api/tags")
         val models = response.body<OllamaModelsResponse>()
+        _modelsStateFlow.value = models.models
+        _selectedModel.value = models.models.firstOrNull()
+
         _historyStateFlow.value += "Server: $models"
     }
 
